e70be7ad2bd8c1e9720091d181f386da7abb574e
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-02-10 01:29:44 +0100

    Fix syntax issues

--- copy-finger.py
+++ copy-finger.py
@@ -7,9 +7,9 @@ from gi.repository import FPrint
 
 
 @click.command()
-@click.argument('fingerprint_path', help='Path to the existing fingerprint file')
-@click.argument('new_user', help='username of the user to add the fingerprint to')
-def copy_fingerprint(fingerpint_path: str, new_user: str):
+@click.argument('fingerprint_path')
+@click.argument('new_user')
+def copy_fingerprint(fingerprint_path: str, new_user: str):
     """
     Tool to copy a fingerprint from one user to another.
 
@@ -17,24 +17,24 @@ def copy_fingerprint(fingerpint_path: str, new_user: str):
     """
     # Duplication code found from tests here:
     # https://github.com/freedesktop/libfprint-fprintd/blob/b440acb57daf0459f2b8b8de82d8284c2040b720/tests/fprintd.py#L827
-    print(f"We will copy fingerprint from {fingerpint_path} to user {new_user}")
+    print(f"We will copy fingerprint from {fingerprint_path} to user {new_user}")
     print("Note that duplicated fingers is NOT SUPPORTED by fprintd for a reason!"
           "Use of this tool may break the ability to identify a user by their fingerprint. No warranty is implied.")
     print("See comment here for why duplicate fingers are not supported:\n"
           "https://gitlab.freedesktop.org/libfprint/fprintd/-/blob/master/src/device.c#L2226")
 
-    with open(fingerpint_path, "rb") as orig_file:
+    with open(fingerprint_path, "rb") as orig_file:
         dup_print = FPrint.Print.deserialize(orig_file.read())
 
     current_user = dup_print.get_username()
-    print(f"Copying {dup_print.get_finger} finger from user {current_user}")
+    print(f"Copying {dup_print.get_finger()} finger from user {current_user}")
     dup_print.set_username(new_user)
     print(f'Successfully applied new username "{new_user}" to print.')
 
     # Reuse path structure from original file.
     # Note that the path is specific to device and finger.
     # We don't support the logic to convert here, nor would that make sense.
-    new_fp_path = fingerpint_path.replace(current_user, new_user)
+    new_fp_path = fingerprint_path.replace(current_user, new_user)
     os.makedirs(os.path.dirname(new_fp_path), exist_ok=True)
 
     with open(new_fp_path, "wb") as new_file:
